1ed742a6e9c3b05073b66d6e1765b86914121847
KarolMaliglowka <38730736+KarolMaliglowka@users.noreply.github.com>
2018-11-13 12:09:26 +0100

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,20 @@
 from flask import Flask
-app = Flask(__name__)
+from datetime import datetime
 
+app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
     return "<h1>Home Page</h1>"
 
-
 @app.route("/about")
 def about():
     return "<h1>About Page</h1>"
 
+@app.route('/data/', methods=['GET'])
+def date():
+	return str(datetime.now())
 
 if __name__ == '__main__':
     app.run(debug=True)
